3867c20e174782bb113cd194f7c5010c61193a8e
ELHADJ BOUBACAR BAH <com.bouba-dev>
2025-03-21 16:37:23 -0500

    Ajoutd de nouvelle classe Fleur

--- GestionBoutiqueFlorale/Fleur.cs
+++ GestionBoutiqueFlorale/Fleur.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CsvHelper;
+using CsvHelper.Configuration.Attributes;
 
 namespace GestionBoutiqueFlorale
 {
     public class Fleur
     {
+        [Name("Nom")]
         public string Nom { get; set; }
+
+        [Name("Prix Unitaire (CAD)")]
         public double PrixUnitaire { get; set; }
+
+        [Name("Couleur")]
         public string CouleurDominante { get; set; }
+
+        [Name("Caractéristiques")]
         public string Description { get; set; }
 
         public Fleur() { }
